ae7904c1983a24d4b270bcbe316e4f36d09a9d06
Avodey <130594121+Avodey@users.noreply.github.com>
2023-10-18 19:37:58 +0100

    Simplified version fix
    Worked on this today but I'm sad I got beaten to it, the movement system may be an improvement on the main code or the layout could also be good, but unused functions etc. have been removed because I couldn't wrap my head around seeing all of it at once

---
+++ simplified build test/Player.py
@@ -0,0 +1,12 @@
+import pygame 
+
+class Player:
+    def __init__(self, x, y, length, height, image):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.height = height
+        self.image = image
+        self.image = pygame.transform.scale(image, (length, height))
+
+
