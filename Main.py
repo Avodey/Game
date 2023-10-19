e6fefc1a3a7fd3b6dc8005401e70cb63fa0cc1b6
OliverHyde096 <22209628@bucks.ac.uk>
2023-10-19 14:37:24 +0100

    Added working timer code and tidied up code layout

--- Main.py
+++ Main.py
@@ -1,15 +1,29 @@
 import pygame
 from Player import Player
-
+import time
 pygame.init()
+
 # Set the size of the game window
 screen = pygame.display.set_mode((800, 600))
+
 # Name of the game window
 pygame.display.set_caption("First Game")
-#  Clock to allow for smooth movement
+
+# Timer
+timer_font = pygame.font.SysFont('Verdana', 38)
+timer_sec = 60
+timer_text = timer_font.render(time.strftime('%M:%S', time.gmtime(timer_sec)), True, (255, 255, 255))
+
+# Userevent for timer
+timer = pygame.USEREVENT + 1                                                
+pygame.time.set_timer(timer, 1000)    # sets timer with USEREVENT and delay in milliseconds
+
+# Clock to allow for smooth movement
 clock = pygame.time.Clock()
+
 # Speed of the player
 playerSpeed = 200
+
 # Loads the player image and the size of the player
 playerImage = pygame.transform.scale(pygame.image.load("Assets/Guy.png"), (100, 100))
 backgroundImage = pygame.transform.scale(pygame.image.load("Assets/TempBackgr.png"), (screen.get_width(), screen.get_height()))  # Renders the player
@@ -30,7 +44,18 @@ while run:  # Checks for the user trying to quit the game
     """
     timedelta = clock.tick(60)  # Limits screen updating to 60 pixels per second
     timedelta /= 1000  # Converts milliseconds to seconds
+    
+    screen.fill((0, 0, 0))
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == timer: # checks for timer event
+            if timer_sec > 0:
+                timer_sec -= 1
+                timer_text = timer_font.render(time.strftime('%M:%S', time.gmtime(timer_sec)), True, (255, 255, 255))
+            else:
+                pygame.time.set_timer(timer, 0)    # turns off timer event
     # Detection of keyboard inputs
     keys = pygame.key.get_pressed()
 
@@ -50,7 +75,8 @@ while run:  # Checks for the user trying to quit the game
     screen.blit(backgroundImage, (0, 0))  # Renders the background
 
     screen.blit(playerImage, (player.x, player.y))  # Renders the player
-
+    # add another "if timer_sec > 0" here if you want the timer to disappear after reaching 0
+    screen.blit(timer_text, (300, 20))
     pygame.display.update()  # Updates the screen rendering. Only one is needed at any time.
 
 pygame.quit()
