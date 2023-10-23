61c34fab427e3b2863511e51659f7bd4760fa8ca
Toby Hammond <148069215+TobyHammond01@users.noreply.github.com>
2023-10-23 14:04:18 +0100

    Add files via upload

---
+++ TestingBottles/Bottle.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+from pygame.examples.sprite_texture import sprite
+
+sprite = pygame.sprite.Sprite
+class Bottle(pygame.sprite.Sprite):
+    def __init__(self, col, x, y):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 50))
+            self.image = pygame.transform.scale(pygame.image.load("Assets/Alcohol.png"), (100, 100))
+            # self.image.fill(col)
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+
+    def update(self):
+            self.rect.move_ip(0, 5)
+            # check if sprite has gone off screen
+            #if self.rect.top > :
+              #  self.kill()
